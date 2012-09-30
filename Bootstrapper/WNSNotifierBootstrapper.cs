66d7de1a1f81749343a3b54a44ec9f4bca56ef82
Injac <injac@mecum.biz>
2012-09-30 02:59:25 +0200

    Added Prism and created a basic bootstrapper

---
+++ Bootstrapper/WNSNotifierBootstrapper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MahApps.Metro.Controls;
+using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.ServiceLocation;
+
+namespace WNSNotifier.Bootstrapper
+{
+    class WNSNotifierBootstrapper:UnityBootstrapper 
+    {
+        /// <summary>
+        /// Creates the shell (Basic UI for WNSNotifier.
+        /// </summary>
+        /// <returns>The Main Window</returns>
+        protected override System.Windows.DependencyObject CreateShell()
+        {
+            return ServiceLocator.Current.GetInstance<MainWindow>();   
+        }
+
+        /// <summary>
+        /// Initializes the shell.
+        /// </summary>
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            Application.Current.MainWindow = (MetroWindow)this.Shell;
+            Application.Current.MainWindow.Show();
+
+        }
+
+
+    }
+}
